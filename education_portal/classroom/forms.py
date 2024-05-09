1ba030ce2e8875d54a10ea4b631e70594e006709
Artem <temsolv@gmail.com>
2024-05-09 18:37:16 +0900

    fix cards hover

--- education_portal/classroom/forms.py
+++ education_portal/classroom/forms.py
@@ -6,7 +6,7 @@ from classroom.models import Course, Category
 
 class NewCourseForm(forms.ModelForm):
     picture = forms.ImageField(required=True)
-    title = forms.CharField(widget=forms.TextInput(attrs={'class': 'validate'}), required=True)
+    title = forms.CharField(widget=forms.TextInput(attrs={'class': 'validate'}), max_length=40, required=True)
     description = forms.CharField(widget=forms.TextInput(attrs={'class': 'validate'}), required=True)
     syllabus = forms.CharField(widget=CKEditorWidget())
 
