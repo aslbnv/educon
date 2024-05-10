85deeade3155b66fcf6174b8f6b4193c4278de28
Artem <temsolv@gmail.com>
2024-05-11 01:00:49 +0900

    add course assignment date

--- education_portal/learningcontrol/models.py
+++ education_portal/learningcontrol/models.py
@@ -5,6 +5,7 @@ from classroom.models import Course
 class AssignedCourses(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     is_completed = models.BooleanField(default=False)
+    date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.course} - Completed: {self.is_completed}"
