ca24fec6e5e3f534deb3ea319f1b2e155f7661f7
Artem <temsolv@gmail.com>
2024-05-17 01:47:34 +0900

    improve ui on sidebar and assign course page

--- education_portal/learningcontrol/forms.py
+++ education_portal/learningcontrol/forms.py
@@ -6,7 +6,7 @@ from authy.models import Profile
 
 class AssignCourseForm(forms.ModelForm):
     course = forms.ModelChoiceField(queryset=Course.objects.all())
-    due_date = forms.DateTimeField(required=False)
+    due_date = forms.DateTimeField(widget=forms.DateTimeInput(attrs={'type': 'datetime-local'}), required=False)
 
     class Meta:
         model = AssignedCourses
