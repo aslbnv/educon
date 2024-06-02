26aaf9b59ba09a9214df270ec3ca35701e08becc
Artem <temsolv@gmail.com>
2024-06-02 21:37:22 +0900

    update statistics total ui

--- education_portal/education_portal/static/js/init.js
+++ education_portal/education_portal/static/js/init.js
@@ -11,13 +11,9 @@
     $('.modal').modal();
     $('.collapsible').collapsible();
     $('datepicker').datepicker({ format: 'yyyy-mm-dd' });
-    $('.carousel.carousel-slider').carousel({
-      fullWidth: true,
-      indicators: true
-    });
-    $('.carousel.carousel-slider').carousel({
-      fullWidth: true
-    });
-
+    $('.carousel.carousel-slider').carousel({ fullWidth: true, indicators: true });
+    $('.carousel.carousel-slider').carousel({ fullWidth: true });
+    $('.tabs').tabs();
+    $('.tooltipped').tooltip();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
